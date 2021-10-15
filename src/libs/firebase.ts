1247a8cd4800e839540091f18441a7ebaa201e87
hidoshino <91507697+FilipePaiva10@users.noreply.github.com>
2021-10-15 20:01:07 -0300

    Config Firebase

---
+++ src/libs/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_SORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEAINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASEAPPID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const storage = getStorage(firebaseApp);
+
